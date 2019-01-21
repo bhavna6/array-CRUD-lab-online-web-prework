6f75aecb0c72af47c4006f4baa35207fa8de2500
Bhavna C <bc2412@columbia.edu>
2019-01-21 08:02:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  common_animals.pop
+ "giraffes" common_animals.pop
 end
 
 def remove_element_from_start_of_array(array)
