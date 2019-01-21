91ff412728e1e1d990bdf01adccdda4b855cedbe
Bhavna C <bc2412@columbia.edu>
2019-01-21 08:03:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  frogs = common_animals.
+  frogs = common_animals.shift
 
 def retrieve_element_from_index(array, index_number)
   
