ed249e1ab30dde9a5a94d5d103de488703d31e54
Bhavna C <bc2412@columbia.edu>
2019-01-21 08:00:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  [ ]
+  common animals = [ ]
 end
 
 def create_an_array
-  ["dogs", "cats", "birds", "fish"]
+  common animals = ["dogs", "cats", "birds", "fish"]
 end
 
 def add_element_to_end_of_array(array, element)
